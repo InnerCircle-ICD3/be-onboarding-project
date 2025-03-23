73a9717ed486aec9ef9b6c6cc5b3941150bada14
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2025-03-23 23:15:38 +0900

    feat : 설문 받을 항목 갯수 validation 로직 추가

--- project/suchan-onboarding/domain/src/main/java/com/survey/domain/Survey.java
+++ project/suchan-onboarding/domain/src/main/java/com/survey/domain/Survey.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Survey {
+    private static final String SURVEY_OPTIONS_CNT_EXCEPTION_MESSAGE = "설문 받을 항목은 1개 ~ 10개 사이여야 합니다.";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,7 +29,14 @@ public class Survey {
     public Survey(String title, String description, List<SurveyOption> surveyOptions) {
         this.title = title;
         this.description = description;
+        validateSurveyOptionCnt(surveyOptions);
         surveyOptions.forEach(this::addSurveyOption);
+    }
+
+    private void validateSurveyOptionCnt(List<SurveyOption> surveyOptions) {
+        if (surveyOptions.isEmpty() || surveyOptions.size() > 10) {
+            throw new IllegalArgumentException(SURVEY_OPTIONS_CNT_EXCEPTION_MESSAGE);
+        }
     }
 
     private void addSurveyOption(SurveyOption surveyOption) {
