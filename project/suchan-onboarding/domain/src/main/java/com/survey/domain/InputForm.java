9cfd3b9437e3fa51e241f6212c149f5bc16f9911
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2025-03-23 14:09:16 +0900

    feat : domain entity 연관관계 편의 메소드 생성

--- project/suchan-onboarding/domain/src/main/java/com/survey/domain/InputForm.java
+++ project/suchan-onboarding/domain/src/main/java/com/survey/domain/InputForm.java
@@ -1,8 +1,11 @@
 package com.survey.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class InputForm {
 
     @Id
@@ -21,4 +24,39 @@ public class InputForm {
 
     @OneToOne(mappedBy = "inputForm", cascade = CascadeType.ALL, orphanRemoval = true)
     private ChoiceInputForm choiceInputForm;
+
+    public InputForm(String question, TextInputForm textInputForm, ChoiceInputForm choiceInputForm) {
+        validateInputForm(textInputForm, choiceInputForm);
+        this.question = question;
+        addTextInputForm(textInputForm);
+        addChoiceInputForm(choiceInputForm);
+    }
+
+    private void validateInputForm(TextInputForm textInputForm, ChoiceInputForm choiceInputForm) {
+        if (textInputForm == null && choiceInputForm == null) {
+            throw new IllegalArgumentException("최소 하나의 입력 형태는 존재해야 합니다.");
+        }
+
+        if (textInputForm != null && choiceInputForm != null) {
+            throw new IllegalArgumentException("입력 형태가 2가지일 수는 없습니다.");
+        }
+    }
+
+    public void addSurveyOption(SurveyOption surveyOption) {
+        this.surveyOption = surveyOption;
+    }
+
+    private void addTextInputForm(TextInputForm textInputForm) {
+        if (textInputForm != null) {
+            this.textInputForm = textInputForm;
+            textInputForm.addInputForm(this);
+        }
+    }
+
+    private void addChoiceInputForm(ChoiceInputForm choiceInputForm) {
+        if (choiceInputForm != null) {
+            this.choiceInputForm = choiceInputForm;
+            choiceInputForm.addInputForm(this);
+        }
+    }
 }
--- project/suchan-onboarding/domain/src/main/java/com/survey/domain/InputOption.java
+++ project/suchan-onboarding/domain/src/main/java/com/survey/domain/InputOption.java
@@ -2,14 +2,19 @@ package com.survey.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class InputOption {
 
     @Column(nullable = false)
     private String option;
+
+    public InputOption(String option) {
+        this.option = option;
+    }
 }
