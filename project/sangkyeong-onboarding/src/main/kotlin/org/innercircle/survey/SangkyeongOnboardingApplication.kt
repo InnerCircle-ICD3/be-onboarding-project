6bf3bc8ec2bbf6ff71c5afcb628bfd9a02375cb5
DOHA <koo9811@gmail.com>
2025-03-23 16:19:19 +0900

    Fix : Main Rebase 후 PR 위한 Branch

---
+++ project/sangkyeong-onboarding/src/main/kotlin/org/innercircle/survey/SangkyeongOnboardingApplication.kt
@@ -0,0 +1,11 @@
+package org.innercircle.survey
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SangkyeongOnboardingApplication
+
+fun main(args: Array<String>) {
+    runApplication<SangkyeongOnboardingApplication>(*args)
+}
