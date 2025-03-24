f43d163435950b9ec7c9baa9950f04bfa8344eb7
fullth <lth129@naver.com>
2025-03-24 20:29:07 +0900

    feat: 프로젝트 초기 구성

---
+++ taehwan-onboarding/src/main/kotlin/org/survey/taehwanonboarding/TaehwanOnboardingApplication.kt
@@ -0,0 +1,11 @@
+package org.survey.taehwanonboarding
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaehwanOnboardingApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaehwanOnboardingApplication>(*args)
+}
