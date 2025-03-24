367dc002aed68ae47f06308401e92ec3657a9a9d
dhlee3994 <dhlee3994@gmail.com>
2025-03-24 20:39:46 +0900

    Init Project

---
+++ project/donghyeon-onboarding/src/main/java/me/dhlee/donghyeononboarding/DonghyeonOnboardingApplication.java
@@ -0,0 +1,13 @@
+package me.dhlee.donghyeononboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonghyeonOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonghyeonOnboardingApplication.class, args);
+    }
+
+}
