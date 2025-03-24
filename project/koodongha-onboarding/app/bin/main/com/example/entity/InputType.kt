6988fd11a4de1efa1b79659e249892e8e73f7902
kudoha <kudoha@gmail.com>
2025-03-24 18:18:26 +0900

    Chore : 충돌 해결

--- project/koodongha-onboarding/app/bin/main/com/example/entity/InputType.kt
+++
@@ -1,8 +0,0 @@
-package com.example.entity
-
-enum class InputType {
-    SHORT_TEXT,
-    LONG_TEXT,
-    SINGLE_CHOICE,
-    MULTI_CHOICE
-}
--- project/koodongha-onboarding/app/bin/main/com/example/repository/SurveyRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.repository
-
-import com.example.entity.*
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface SurveyRepository : JpaRepository<Survey, Long>
-interface SurveyItemRepository : JpaRepository<SurveyItem, Long>
-interface SelectionOptionRepository : JpaRepository<SelectionOption, Long>
