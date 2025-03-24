11d3f3788b2fc1a2adcc3aaca9b7c6fd3f5e1859
Levi(임태환) <thim@gabia.com>
2025-03-24 23:04:57 +0900

    feat: 불필요한 allOpen 설정 제거

--- project/taehwan-onboarding/build.gradle.kts
+++ project/taehwan-onboarding/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint").version("12.1.1")
-    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "org.survey"
@@ -43,12 +43,6 @@ kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
     }
-}
-
-allOpen {
-    annotation("jakarta.persistence.Entity")
-    annotation("jakarta.persistence.MappedSuperclass")
-    annotation("jakarta.persistence.Embeddable")
 }
 
 tasks.withType<Test> {
