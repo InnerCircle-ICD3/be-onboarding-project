63af1449c873700cfef9938379f9863bfd5f58d8
suzhanlee <wlscww@kakao.com>
2025-03-24 21:06:58 +0900

    test : Survey test code 작성

--- project/suchan-onboarding/domain/src/test/java/com/survey/domain/SurveyTest.java
+++ project/suchan-onboarding/domain/src/test/java/com/survey/domain/SurveyTest.java
@@ -1,6 +1,5 @@
 package com.survey.domain;
 
-import com.survey.test.TestFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
