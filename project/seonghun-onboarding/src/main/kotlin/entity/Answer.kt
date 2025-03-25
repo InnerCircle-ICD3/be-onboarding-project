59197651730ce68b18e0b4f9439eabddd67b9c7e
CHOSSEANG <jng00153@gmail.com>
2025-03-25 20:07:12 +0900

    chore: Answer , Item, Survey entity 생성

---
+++ project/seonghun-onboarding/src/main/kotlin/entity/Answer.kt
@@ -0,0 +1,36 @@
+package entity
+
+import jakarta.persistence.CollectionTable
+import jakarta.persistence.ElementCollection
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+data class Answer(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    val name: String,
+
+    @ElementCollection
+    @CollectionTable(
+        name = "answer_items",
+        joinColumns = [JoinColumn(name = "answer_id")]
+    )
+    val items: List<String>,
+
+    @ElementCollection
+    @CollectionTable(
+        name = "answer_responses",
+        joinColumns = [JoinColumn(name = "answer_id")]
+    )
+    val responses: List<String>,
+
+    @ManyToOne
+    @JoinColumn(name = "survey_id")
+    val survey: Survey
+)
---
+++ project/seonghun-onboarding/src/main/kotlin/entity/Item.kt
@@ -0,0 +1,28 @@
+package entity
+
+import enum.ItemType
+import jakarta.persistence.CollectionTable
+import jakarta.persistence.ElementCollection
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+
+@Entity
+data class Item(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    val name: String,
+    val description: String,
+    val type: Enum<ItemType>,
+
+    @ElementCollection
+    @CollectionTable(
+        name = "item_contents",
+        joinColumns = [JoinColumn(name = "itemId")]
+    )
+    val contents: List<String>
+)
