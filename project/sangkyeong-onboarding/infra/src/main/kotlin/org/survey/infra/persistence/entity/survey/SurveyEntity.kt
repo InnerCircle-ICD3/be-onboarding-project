5b578f1503d2ebe2c2ba9af25fb32181a6740f64
Lee Sang kyeong <sangk3758@gmail.com>
2025-03-25 20:59:19 +0900

    feat: 설문조사 도메인 Entity 클래스 정의

---
+++ project/sangkyeong-onboarding/infra/src/main/kotlin/org/survey/infra/persistence/entity/survey/SurveyEntity.kt
@@ -0,0 +1,29 @@
+package org.survey.infra.persistence.entity.survey
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "survey")
+class SurveyEntity(
+    title: String,
+    description: String,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
+
+    var title: String = title
+        protected set
+
+    var description: String = description
+        protected set
+
+    @Column(nullable = false, updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+        protected set
+
+    @Column(nullable = false)
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+        protected set
+}
