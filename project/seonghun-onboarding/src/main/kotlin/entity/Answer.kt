1d64627eb88d5a13a98a46bda97224877dd35781
CHOSSEANG <jng00153@gmail.com>
2025-03-26 21:54:18 +0900

    chore: Answer 클래스에 Entity 어노테이션 추가

--- project/seonghun-onboarding/src/main/kotlin/entity/Answer.kt
+++ project/seonghun-onboarding/src/main/kotlin/entity/Answer.kt
@@ -2,6 +2,7 @@ package entity
 
 import jakarta.persistence.CollectionTable
 import jakarta.persistence.ElementCollection
+import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -9,6 +10,7 @@ import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 
+@Entity
 data class Answer(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
