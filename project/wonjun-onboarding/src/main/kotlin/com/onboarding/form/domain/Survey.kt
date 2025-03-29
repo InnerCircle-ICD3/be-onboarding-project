5d7676be48ce904bbb96034c2d164a6ffa55c280
backend-wonjun-choi <wonjun.choi@a-bly.com>
2025-03-29 10:45:16 +0900

    Survey UnitTest 에도 특정 수 이상 Question 등록시 에러가 발생하는지 확인하는 테스트 추가

--- project/wonjun-onboarding/src/main/kotlin/com/onboarding/form/domain/Survey.kt
+++ project/wonjun-onboarding/src/main/kotlin/com/onboarding/form/domain/Survey.kt
@@ -12,7 +12,7 @@ class Survey (
     @OneToMany(mappedBy = "survey", cascade = [CascadeType.ALL], orphanRemoval = true)
     val questions: MutableList<Question> = mutableListOf()
 ){
-    fun addItem(question: Question){
+    fun addQuestion(question: Question){
         check(questions.size < MAX_QUESTION_SIZE) { "The number of questions cannot exceed $MAX_QUESTION_SIZE" }
         question.survey = this
         this.questions.add(question)
