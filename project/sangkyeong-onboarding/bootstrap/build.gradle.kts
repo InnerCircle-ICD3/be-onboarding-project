d897481ba6a441891552a92229455c3f275bb2c5
Lee Sang kyeong <sangk3758@gmail.com>
2025-03-29 17:51:47 +0900

    fix: 빌드 오류 해결

--- project/sangkyeong-onboarding/bootstrap/build.gradle.kts
+++ project/sangkyeong-onboarding/bootstrap/build.gradle.kts
@@ -1,6 +1,12 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 dependencies {
     implementation(project(":presentation"))
     implementation(project(":application"))
     implementation(project(":infra"))
     implementation(project(":domain"))
+}
+
+tasks.withType<BootJar> {
+    enabled = true
 }
