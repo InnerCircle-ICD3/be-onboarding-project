9ec12e6440361fe92dcab8b4a0290b5a8e9b4ec7
youngDaLee <ldy0956@gmail.com>
2025-03-31 22:17:44 +0900

    feat: 빌드 구성

--- project/dayoung-onboarding/onboarding-data/build.gradle.kts
+++ project/dayoung-onboarding/onboarding-data/build.gradle.kts
@@ -2,3 +2,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
 }
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
+}
