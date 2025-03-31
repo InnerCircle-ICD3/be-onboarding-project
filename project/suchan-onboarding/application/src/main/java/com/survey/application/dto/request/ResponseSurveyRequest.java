2a66f3f66c5caf858b0d13c96bb62f51dc0e05b1
suzhanlee <wlscww@kakao.com>
2025-03-31 09:37:23 +0900

    feat : Survey 와 SurveyResponse 의 상호작용 validation을 처리하는 도메인 서비스 기능 추가 및 Request 객체 추가

---
+++ project/suchan-onboarding/application/src/main/java/com/survey/application/dto/request/ResponseSurveyRequest.java
@@ -0,0 +1,98 @@
+package com.survey.application.dto.request;
+
+import com.survey.domain.survey.ChoiceType;
+import com.survey.domain.survey.TextType;
+import com.survey.domain.surveyResponse.ChoiceResponse;
+import com.survey.domain.surveyResponse.SurveyOptionResponse;
+import com.survey.domain.surveyResponse.SurveyResponse;
+import com.survey.domain.surveyResponse.TextResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseSurveyRequest {
+    @NotNull
+    private Long surveyId;
+
+    @NotNull
+    private Long surveyVersion;
+
+    @NotNull
+    private LocalDateTime submittedAt;
+
+    @NotNull
+    @NotEmpty
+    @Valid
+    private List<SurveyOptionResponseDto> optionResponses;
+
+    public SurveyResponse create() {
+        return new SurveyResponse(
+                surveyId,
+                surveyVersion,
+                submittedAt,
+                optionResponses.stream()
+                        .map(SurveyOptionResponseDto::create)
+                        .toList());
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class SurveyOptionResponseDto {
+        @NotNull
+        private Long surveyOptionId;
+
+        @Valid
+        private TextResponseDto textResponseDto;
+
+        @Valid
+        private ChoiceResponseDto choiceResponseDto;
+
+        public SurveyOptionResponse create() {
+            if (textResponseDto != null) {
+                return new SurveyOptionResponse(surveyOptionId, textResponseDto.create());
+            }
+            return new SurveyOptionResponse(surveyOptionId, choiceResponseDto.create());
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class TextResponseDto {
+        @NotNull
+        private String textType;
+
+        @NotNull
+        private String textAnswer;
+
+        public TextResponse create() {
+            return new TextResponse(TextType.findByName(textType), textAnswer);
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class ChoiceResponseDto {
+        @NotNull
+        private String choiceType;
+
+        @NotNull
+        @NotEmpty
+        private List<String> selectedOptions;
+
+        public ChoiceResponse create() {
+            return new ChoiceResponse(ChoiceType.findByName(choiceType), selectedOptions);
+        }
+    }
+}
---
+++ project/suchan-onboarding/domain/src/main/java/com/survey/domain/surveyResponse/repository/SurveyResponseRepository.java
@@ -0,0 +1,7 @@
+package com.survey.domain.surveyResponse.repository;
+
+import com.survey.domain.surveyResponse.SurveyResponse;
+
+public interface SurveyResponseRepository {
+    void save(SurveyResponse surveyResponse);
+}
