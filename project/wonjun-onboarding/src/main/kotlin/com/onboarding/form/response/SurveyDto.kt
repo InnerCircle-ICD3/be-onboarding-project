1181fe8eeaee55d3ca238eec8cd0ccfb488448ab
backend-wonjun-choi <wonjun.choi@a-bly.com>
2025-04-02 20:51:10 +0900

    Survey Response에 Version 추가

--- project/wonjun-onboarding/src/main/kotlin/com/onboarding/form/response/SurveyDto.kt
+++ project/wonjun-onboarding/src/main/kotlin/com/onboarding/form/response/SurveyDto.kt
@@ -1,12 +1,29 @@
 package com.onboarding.form.response
 
+import com.onboarding.form.domain.Survey
+
 
 data class SurveyDto(
     val id: Long,
+    val version: Int,
     val title: String,
     val description: String,
     val item: List<QuestionDto>
 ) {
+
+    companion object {
+        fun of(survey: Survey): SurveyDto {
+            return SurveyDto(
+                survey.id!!,
+                survey.currentVersion.version,
+                survey.title,
+                survey.description,
+                survey.getQuestions().map {
+                    QuestionDto.of(it)
+                }.toList()
+            )
+        }
+    }
 
 }
 
