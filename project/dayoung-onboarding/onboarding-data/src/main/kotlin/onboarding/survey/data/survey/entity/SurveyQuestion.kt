890a44c8b4e6da11cdb25058ecee8992fee971a1
youngDaLee <ldy0956@gmail.com>
2025-04-03 22:17:07 +0900

    feat: 설문 항목 버전 관리

--- project/dayoung-onboarding/onboarding-data/src/main/kotlin/onboarding/survey/data/survey/entity/SurveyQuestion.kt
+++ project/dayoung-onboarding/onboarding-data/src/main/kotlin/onboarding/survey/data/survey/entity/SurveyQuestion.kt
@@ -1,7 +1,9 @@
 package onboarding.survey.data.survey.entity
 
 import jakarta.persistence.*
+import onboarding.survey.data.survey.type.SurveyQuestionStatus
 import onboarding.survey.data.survey.type.SurveyQuestionType
+import java.util.Date
 
 @Entity
 @Table(name = "SURVEY_QUESTION")
@@ -21,5 +23,9 @@ data class SurveyQuestion(
 
     @Enumerated(EnumType.STRING)
     @Column(name = "question_type")
-    val questionType: SurveyQuestionType
+    val questionType: SurveyQuestionType,
+
+    val questionStatus: SurveyQuestionStatus = SurveyQuestionStatus.ACTIVE,
+    val lastModifiedDate: Date = Date(),
+    val version: Int = 1
 )
