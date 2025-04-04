f16ba2e5a2bd9d4fc445016089d054cff1e2a163
hj4645 <hjpark4645@gmail.com>
2025-04-05 07:33:20 +0900

    feat: logging + spy 추가

--- project/hjpark-onboarding/survey/api/src/main/kotlin/com/hjpark/survey/SurveyApplication.kt
+++ project/hjpark-onboarding/survey/api/src/main/kotlin/com/hjpark/survey/SurveyApplication.kt
@@ -2,11 +2,10 @@ package com.hjpark.survey
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 class SurveyApplication
 
 fun main(args: Array<String>) {
     runApplication<SurveyApplication>(*args)
-} 
+}
