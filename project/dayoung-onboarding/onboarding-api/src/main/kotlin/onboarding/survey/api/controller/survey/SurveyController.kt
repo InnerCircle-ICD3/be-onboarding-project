94782880392da6ee99cd4c2c797916b5d0a42a34
youngDaLee <ldy0956@gmail.com>
2025-04-05 01:38:13 +0900

    feat: 설문 응답 제출 api 추가

--- project/dayoung-onboarding/onboarding-api/src/main/kotlin/onboarding/survey/api/controller/survey/SurveyController.kt
+++ project/dayoung-onboarding/onboarding-api/src/main/kotlin/onboarding/survey/api/controller/survey/SurveyController.kt
@@ -9,6 +9,7 @@ import onboarding.survey.api.model.response.AnswerSurveyResponse
 import onboarding.survey.api.model.response.CreateSurveyResponse
 import onboarding.survey.api.model.response.GetAnswersResponse
 import onboarding.survey.api.model.response.UpdateSurveyResponse
+import onboarding.survey.api.service.survey.SurveyAnswerService
 import onboarding.survey.api.service.survey.SurveyService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -16,7 +17,8 @@ import org.springframework.web.bind.annotation.*
 @Tag(name = "Survey", description = "설문조사 관련 CRUD API")
 @RestController
 class SurveyController(
-    private val surveyService: SurveyService  // ✅ 생성자 주입
+    private val surveyService: SurveyService,
+    private val surveyAnswerService: SurveyAnswerService
 ) {
     @Operation(summary = "설문조사 생성")
     @PostMapping("/survey")
@@ -41,8 +43,8 @@ class SurveyController(
         @PathVariable surveyId: Int,
         @RequestBody request: AnswerSurveyRequest
     ): ResponseEntity<AnswerSurveyResponse> {
-        // TODO: 서비스 호출 및 저장 처리
-        return ResponseEntity.ok(AnswerSurveyResponse(surveyId = surveyId, answerId = 1))
+        val response = surveyAnswerService.answerSurvey(surveyId, request)
+        return ResponseEntity.ok(response)
     }
 
     @Operation(summary = "설문 응답 전체 조회")
