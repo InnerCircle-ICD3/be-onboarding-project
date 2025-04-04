5ee40022d63be024f22d58e139eb135aed9d0fb4
dhlee3994 <dhlee3994@gmail.com>
2025-04-05 01:38:28 +0900

    refactor: 비선택형 항목에 대한 예외 메시지 수정

--- project/donghyeon-onboarding/src/main/java/me/dhlee/donghyeononboarding/exception/ErrorCode.java
+++ project/donghyeon-onboarding/src/main/java/me/dhlee/donghyeononboarding/exception/ErrorCode.java
@@ -18,7 +18,7 @@ public enum ErrorCode {
     SURVEY_ITEM_OPTION_TITLE_IS_EMPTY("설문조사 항목 옵션 제목은 필수 입력사항입니다."),
     SURVEY_ITEM_OPTION_IS_EMPTY("설문조사 항목 옵션은 필수 입력사항입니다."),
     SURVEY_ITEM_OPTION_SIZE_OVERFLOW("설문조사 항목 옵션은 최대 10개까지 가능합니다."),
-    NOT_SELECTABLE_ITEM_TYPE_SHOULD_HAVE_NOT_OPTIONS("단답형, 장문형 항목은 설문조사 항목을 가질 수 없습니다."),
+    NOT_SELECTABLE_ITEM_TYPE_SHOULD_HAVE_NOT_OPTIONS("비선택형 항목은 설문조사 항목을 가질 수 없습니다."),
 
     DISPLAY_ORDER_IS_NEGATIVE("항목 표시순서는 0 이상이어야 합니다."),
 
