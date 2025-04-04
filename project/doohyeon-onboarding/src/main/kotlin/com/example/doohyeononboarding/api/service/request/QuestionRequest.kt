a1034c9f1c0acc93ab5c7278ae19d92f9ffed8db
GwonDooHyeon <secure3141@gmail.com>
2025-04-05 07:52:12 +0900

    feat: 설문조사 등록 API 구현

---
+++ project/doohyeon-onboarding/src/main/kotlin/com/example/doohyeononboarding/api/service/request/QuestionRequest.kt
@@ -0,0 +1,18 @@
+package com.example.doohyeononboarding.api.service.request
+
+import com.example.doohyeononboarding.domain.question.QuestionType
+
+class QuestionRequest(
+    val title: String,
+    val description: String? = "",
+    val type: QuestionType,
+    val isRequired: Boolean,
+    val options: MutableList<String>? = null // 선택형일 때만 필요
+) {
+
+    init {
+        require(title.isNotBlank()) { "질문 제목은 필수입니다." }
+    }
+
+}
+
