40e88e7b540c0ff9bc954ac7c7d3a0cc79a56d97
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2025-04-05 09:13:08 +0900

    API Docs 추가 (#117)
    * add open api docs
    
    * add open api docs description

--- project/kyungtae-onboarding/survey-api/src/main/kotlin/com/innercircle/api/common/GlobalExceptionHandler.kt
+++ project/kyungtae-onboarding/survey-api/src/main/kotlin/com/innercircle/api/common/GlobalExceptionHandler.kt
@@ -1,7 +1,7 @@
 package com.innercircle.api.common
 
-import com.innercircle.api.common.response.ApiResponse
 import com.innercircle.api.common.response.ApiResponseCode
+import com.innercircle.api.common.response.ApiResponseDto
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -13,23 +13,23 @@ class GlobalExceptionHandler : Log {
 
     @ExceptionHandler(IllegalArgumentException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handleIllegalArgument(ex: IllegalArgumentException): ResponseEntity<ApiResponse<Unit>> {
+    fun handleIllegalArgument(ex: IllegalArgumentException): ResponseEntity<ApiResponseDto<Any>> {
         log.error("Bad request", ex)
-        return ResponseEntity.ok().body(ApiResponse.error(ApiResponseCode.INVALID_ARGUMENT))
+        return ResponseEntity.ok().body(ApiResponseDto.error(ApiResponseCode.INVALID_ARGUMENT))
     }
 
     @ExceptionHandler(NoSuchElementException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    fun handleNotFound(ex: NoSuchElementException): ResponseEntity<ApiResponse<Unit>> {
+    fun handleNotFound(ex: NoSuchElementException): ResponseEntity<ApiResponseDto<Any>> {
         log.error("Resource not found", ex)
-        return ResponseEntity.ok().body(ApiResponse.error(ApiResponseCode.NOT_FOUND))
+        return ResponseEntity.ok().body(ApiResponseDto.error(ApiResponseCode.NOT_FOUND))
     }
 
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun handleGeneric(ex: Exception): ResponseEntity<ApiResponse<Unit>> {
+    fun handleGeneric(ex: Exception): ResponseEntity<ApiResponseDto<Any>> {
         log.error("Internal server error", ex)
-        return ResponseEntity.ok().body(ApiResponse.error(ApiResponseCode.INTERNAL_ERROR))
+        return ResponseEntity.ok().body(ApiResponseDto.error(ApiResponseCode.INTERNAL_ERROR))
     }
 
 }
--- project/kyungtae-onboarding/survey-api/src/main/kotlin/com/innercircle/api/survey/controller/request/SurveyQuestionOptionCreateRequest.kt
+++ project/kyungtae-onboarding/survey-api/src/main/kotlin/com/innercircle/api/survey/controller/request/SurveyQuestionOptionCreateRequest.kt
@@ -1,8 +1,10 @@
 package com.innercircle.api.survey.controller.request
 
 import com.innercircle.domain.survey.command.dto.SurveyQuestionOptionCreateCommand
+import io.swagger.v3.oas.annotations.media.Schema
 
 data class SurveyQuestionOptionCreateRequest(
+    @Schema(description = "답변 옵션 내용", example = "순대국", required = true)
     val content: String? = null,
 ) {
     fun toCommand(): SurveyQuestionOptionCreateCommand {
